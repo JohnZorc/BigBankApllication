31aae9de04ad90f53fc7e2335ee1157b51b82df1
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2020-10-23 23:20:09 -0400

    Integrated DB.

--- src/main/java/com/example/demo/BigBankApplication.java
+++ src/main/java/com/example/demo/BigBankApplication.java
@@ -1,9 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.schemas.Log;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 @SpringBootApplication
 @RestController
@@ -12,6 +27,17 @@ public class BigBankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BigBankApplication.class, args);
 	}
+
+	// Setting up DB
+	CodecRegistry pojoCodecRegistry = fromRegistries(
+			MongoClientSettings.getDefaultCodecRegistry(),
+			fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+	MongoClientSettings settings = MongoClientSettings.builder()
+			.codecRegistry(pojoCodecRegistry)
+			.build();
+	MongoClient mongoClient = MongoClients.create(settings); // Connects to mongoDB deamon running on port 27017
+	MongoDatabase database = mongoClient.getDatabase("big-bank-db"); // Gets db from deamon, creates it if not found.
+
 
 	@PostMapping("/SimpleSavings")
 	public String SimpleSavings(@RequestBody String SScalc) {
@@ -70,5 +96,45 @@ public class BigBankApplication {
 		String returnString = testObject.toString();
 		return returnString;
 
+	}
+
+	@PostMapping("/AddLog")
+	public String AddLog() {
+		/*
+		* Team here are the instructions on how to use the DB
+		* 	1. Get your collection
+		* 	2. Perform CRUD operations on it (look at link for example).
+		* */
+
+		// Here is how to create a `Log` object and save it on the db.
+		// Step 1:
+		MongoCollection<Log> logs = database.getCollection("log", Log.class);
+
+		// Step 2:
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		Log example = new Log(timestamp.toString(), "request","/DBExample", "sampleAPIkey");
+		logs.insertOne(example);
+
+		return example.toString();
+	}
+
+	@GetMapping("/GetAllLogs")
+	public List<String> GetAllLogs() {
+		MongoCollection<Document> logs = database.getCollection("log");
+		List<String> response = new ArrayList<String>();
+
+		Consumer<Document> addLogToList = new Consumer<Document>() {
+			@Override
+			public void accept(final Document document) {
+				response.add(document.toJson());
+			}
+		};
+
+
+		logs.find().forEach(addLogToList);
+
+
+
+		return response;
 	}
 }
---
+++ src/main/java/com/example/demo/schemas/Log.java
@@ -0,0 +1,36 @@
+package com.example.demo.schemas;
+
+import org.bson.types.ObjectId;
+
+public class Log {
+
+    private ObjectId id;
+
+    // Properties
+    public String timestamp;
+    public String requestOrResponse;
+    public String endpoint;
+    public String APIKeyUsed;
+
+
+
+    // Constructor (you can have multiple)
+    public Log(String timestamp, String requestOrResponse, String endpoint, String APIKeyUsed){
+        this.timestamp = timestamp;
+        this.requestOrResponse = requestOrResponse;
+        this.endpoint = endpoint;
+        this.APIKeyUsed = APIKeyUsed;
+    }
+
+    public Log(){}
+
+
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Log[id=%s, timestamp='%s', requestOrResponse='%s', endpoint='%s', APIKeyUsed='%s']",
+                id, timestamp, requestOrResponse, endpoint, APIKeyUsed);
+    }
+
+}
