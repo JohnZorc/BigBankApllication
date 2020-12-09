f5e68f4a8c317fe01dabb9026e507dfd2818a2c7
Nathan <schwartznathan99@gmail.com>
2020-12-08 20:08:06 -0500

    Added the Login and Registration pages

--- src-frontend/src/pages/Login.js
+++ src-frontend/src/pages/Login.js
@@ -1,16 +1,79 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import {Container, Form} from 'semantic-ui-react'
+import { Button, Card } from 'semantic-ui-react'
+import axios from 'axios'
+import Redirect from "react-router-dom/es/Redirect";
+import styles from "../styles";
+import '../main.css'
 
-export default class Login extends React.Component {
-    constructor(props) {
-        super(props);
-      }
+function Login() {
 
-    render() {
+        const [form, setForm] = React.useState({
+               email: '',
+               password: ''
+            });
+            const [registrationStatus, setRegistrationStatus] = React.useState(false);
+
+                const handleChange = (e, data) => {
+                    let name, value;
+                    if(data){
+                        name = data.name;
+                        value = data.value;
+                    } else {
+                        name = e.target.name;
+                        value = e.target.value;
+                    }
+
+                    setForm({...form, [name]: value});
+                };
+                const handleSubmit = (e) => {
+                    e.preventDefault();
+                    axios.post(`http://localhost:8080/login`, form)
+                        .then(() => {
+                            alert("User was successfully created");
+                            setRegistrationStatus(true);
+                        })
+                        .catch(() => {
+                            alert("An error occurred while logging in!");
+                            setRegistrationStatus(false);
+                        })
+                };
+
+                if(registrationStatus){
+                        return (<Redirect to={'/Dashboard'}/>)
+                    }
+
         return(
-            <div>
-                {"hi"}
-            </div>
-        )
-    }
+            <Container style ={styles.container}>
+                        <Card style={styles.card}>
+                            <Card.Content style={styles.content}>
+                                <Card.Header>
+                                    <h1 style={styles.heading}>Login</h1>
+                                </Card.Header>
+                                <Card.Description>
+                                    <Form>
+                                        <Form.Field>
+                                            <label>E-mail Address</label>
+                                            <input required name={'email'} placeholder='E-mail Address' onChange={handleChange} value={form.email}/>
+                                        </Form.Field>
+                                        <Form.Field>
+                                            <label>Password</label>
+                                            <input name={'password'} type={'password'} placeholder='Password' onChange={handleChange} value={form.password}/>
+                                        </Form.Field>
+                                    </Form>
+                                </Card.Description>
+
+                                <div style={styles.buttons}>
+                                    <Link to={'/register'}>
+                                        <Button color={'red'} style={styles.button}>Register</Button>
+                                    </Link>
+                                        <Button color={'blue'} style={styles.button} onClick={handleSubmit}>Submit</Button>
+                                </div>
+                            </Card.Content>
+                        </Card>
+                    </Container>
+        );
 
 }
+export default Login;
---
+++ src-frontend/src/styles.js
@@ -0,0 +1,61 @@
+const styles = {
+    container: {
+        height: "100vh",
+    },
+    card_client: {
+        height: "auto",
+        width:"auto",
+        margin: '15% auto',
+        textAlign: 'center',
+        padding: '25px'
+    },
+    card: {
+        margin: '15% auto',
+        width: '80%',
+        height: 'auto',
+        textAlign: 'center',
+        padding: '25px'
+    },
+    content: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    heading: {
+        fontSize: '45px'
+    },
+    mainText: {
+        fontSize: '20px'
+    },
+    buttons: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '30%',
+        margin: '25px auto',
+    },
+    button: {
+        fontSize: '17px'
+    },
+    button_Admin:{
+        fontSize: '15px',
+        marginBottom: '1em',
+    },
+    small_Button: {
+        width: '50px',
+
+    },
+    cloud_Button: {
+        height: '37px',
+        position: 'relative',
+        right: '-20px',
+
+    },
+    form: {
+        textAlign: 'left'
+    },
+    tableText: {
+        fontSize:'14px',
+    },
+};
+export default styles;
