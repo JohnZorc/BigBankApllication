bb3437d43e75b7df54a8f111941a246c58364359
John Zorc <johnny.zorc@gmail.com>
2020-12-09 18:08:00 -0500

    Added APIKey interceptor to CFI functions, added /login and /register /createLog and /GetAllLogs

--- src/main/java/com/example/demo/schemas/Customer.java
+++ src/main/java/com/example/demo/schemas/Customer.java
@@ -5,30 +5,39 @@ import org.bson.types.ObjectId;
 public class Customer
 {
 
-  private ObjectId id;
+  public ObjectId id;
 
   public String firstName;
   public String lastName;
   public String emailAddress;
   public String homeAddress;
-  public int password;
+  public int  password;
+  public int APIKey;
 
-  public Customer(String firstName, String lastName, String emailAddress, String homeAddress, int password) {
+  public Customer(String firstName, String lastName, String emailAddress, String homeAddress, int password, int APIKey) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.emailAddress = emailAddress;
     this.homeAddress = homeAddress;
     this.password = password;
+    this.APIKey = APIKey;
   }
 
   public Customer() {
 
   }
+  public String getID() {
+    return id.toString();
+  }
 
   @Override
   public String toString() {
     return String.format(
-        "Customer[firstname = %s, lastname = %s, emailAddress = %s, homeaddress = %s, password = %s]",
-        this.firstName, this.lastName, this.emailAddress, this.homeAddress, this.password);
+        "Customer[firstname = %s, lastname = %s, emailAddress = %s, homeaddress = %s, password = %s, APIKey = %s]",
+        this.firstName, this.lastName, this.emailAddress, this.homeAddress, this.password, this.APIKey);
+  }
+
+  public int getAPIKey() {
+    return APIKey;
   }
 }
