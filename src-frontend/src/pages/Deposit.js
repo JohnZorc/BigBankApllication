41794ceb9a7fb757a499d7328f1e99736afe4eb4
Monica Tam <monicatam75@hotmail.com>
2020-12-12 16:06:06 -0500

    Update Deposit.js

--- src-frontend/src/pages/Deposit.js
+++ src-frontend/src/pages/Deposit.js
@@ -6,16 +6,16 @@ export default function Dashboard(props)  {
 
     React.useEffect(() => {
 
-        if(props.token===""){
-            props.history.replace({pathname: '/login'});
-        }
+//         if(props.token===""){
+//             props.history.replace({pathname: '/login'});
+//         }
 
-        axios.get(`http://localhost:8080/dashboard/`,{headers:{Authorization:props.token}})
-        .then(res => {
-            if(res.data==="You do not have access to access this page."){
-                props.history.replace({pathname: '/login'});
-            }
-        })
+//         axios.get(`http://localhost:8080/dashboard/`,{headers:{Authorization:props.token}})
+//         .then(res => {
+//             if(res.data==="You do not have access to access this page."){
+//                 props.history.replace({pathname: '/login'});
+//             }
+//         })
     });
 
     const { register, errors, handleSubmit } = useForm({
