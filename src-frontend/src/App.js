ba25f18c608d7a30b6bc327785d2fff63b479750
Monica Tam <monicatam75@hotmail.com>
2020-12-12 16:03:35 -0500

    Update App.js

--- src-frontend/src/App.js
+++ src-frontend/src/App.js
@@ -25,7 +25,8 @@ function App() {
           <Route exact path="/new_account" render={(props)=> <CreateAcc {...props} token={jwtToken} customer={customer}/>}/>
           <Route exact path="/transfer" render={(props)=> <Transfer {...props} token={jwtToken} customer={customer}/>}/>
           <Route exact path="/">
-              <Redirect to="/login" />
+//               <Redirect to="/login" />
+              <Redirect to="/dashboard" />
           </Route>
         </Switch>
       </BrowserRouter>
